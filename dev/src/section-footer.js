bcec82fd6c3b506555d2f7f3c8f8c993df44ec29
diana-pv-dev <diana@refactorplus.com>
2023-12-22 12:30:44 +0200

    fix: footer logic

---
+++ dev/src/section-footer.js
@@ -0,0 +1,36 @@
+class FooterAccordion extends HTMLElement {
+  activeMenu = null;
+
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.openButtons = this.querySelectorAll('.js-footer-menu__button')
+    this.buttonChevron = this.querySelectorAll('.js-footer-menu__icon')
+
+    for (const openButton of this.openButtons) {
+      openButton.addEventListener('click', (event) => this.toggleMenu(event))
+    }
+  }
+
+  disconnectedCallback() {
+    for (const openButton of this.openButtons) {
+      openButton.removeEventListener('click', (event) => this.toggleMenu(event))
+    }
+  }
+
+  toggleMenu(event) {
+    if (this.activeMenu !== null && this.activeMenu !== event.currentTarget) {
+      this.activeMenu.nextElementSibling.classList.remove('footer-menu__accordion-content--active')
+      this.activeMenu.firstElementChild.classList.remove('footer-menu__icon--active');
+    }
+
+    this.activeMenu = event.currentTarget;
+
+    this.activeMenu.nextElementSibling.classList.toggle('footer-menu__accordion-content--active');
+    this.activeMenu.firstElementChild.classList.toggle('footer-menu__icon--active');
+  }
+}
+
+customElements.define('footer-accordion', FooterAccordion)

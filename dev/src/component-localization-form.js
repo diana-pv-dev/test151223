5183a17c1919add9ddb35a3ddd83b837604a584e
diana-pv-dev <diana@refactorplus.com>
2023-12-25 11:27:54 +0200

    fix: localization and currency

--- dev/src/component-localization-form.js
+++ dev/src/component-localization-form.js
@@ -1,3 +1,6 @@
+
+// localization form is used in the menu (menu-list.liquid)
+// its a custom selector (button + ul li)
 class LocalizationForm extends HTMLElement {
   constructor() {
     super();
@@ -47,33 +50,45 @@ class LocalizationForm extends HTMLElement {
   //   const form = this.closest('form');
   //   if (form) form.submit();
   // }
-
 }
 
-class LocalizationSelect extends HTMLElement {
+// localization form is used in the header
+// its a regular selector (select + options)
+class LocalizationSelectForm extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
-    this.input = this.querySelector('.js-localization__input')
+    this.inputLang = this.querySelector('.js-localization__input--lang')
     this.selectLang = this.querySelector('.js-localization__select')
+    this.inputCurrency = this.querySelector('.js-localization__input--currency')
+    this.selectCurrency = this.querySelector('.js-currency__select')
 
-    this.selectLang.addEventListener('change', this.onSelectChange.bind(this));
+    this.selectLang.addEventListener('change', this.onLangChange.bind(this));
+    this.selectCurrency.addEventListener('change', this.onCurrencyChange.bind(this));
   }
 
   disconnectedCallback() {
-    this.selectLang.removeEventListener('change', this.onSelectChange.bind(this));
+    this.selectLang.removeEventListener('change', this.onLangChange.bind(this));
+    this.selectCurrency.removeEventListener('change', this.onCurrencyChange.bind(this));
   }
   
-  onSelectChange(event) {
+  onLangChange(event) {
     const form = this.querySelector('form');
-    console.log(form)
-    this.input.value = event.currentTarget.value;
+
+    this.inputLang.value = event.currentTarget.value;
+    if (form) form.submit();
+  }
+
+  onCurrencyChange(event) {
+    const form = this.querySelector('form');
+
+    this.inputCurrency.value = event.currentTarget.value;
     if (form) form.submit();
   }
 
 }
 
 customElements.define('localization-form', LocalizationForm);
-customElements.define('localization-select', LocalizationSelect);
+customElements.define('localization-select-form', LocalizationSelectForm);
